aff4b7dcce5210a218c90d2107ced84b142ada27
janmal8359 <alsrl0033@gmail.com>
2021-09-02 18:44:21 +0900

    오프닝씬 재정의 및 내용 추가

--- openingScene.cpp
+++ openingScene.cpp
@@ -11,6 +11,8 @@ openingScene::~openingScene()
 
 HRESULT openingScene::init()
 {
+	opening = IMAGEMANAGER->addFrameImage("¿ÀÇÁ´×", "image/op_image_frame.bmp", 65280, 576, 102, 1, true, RGB(255, 0, 255));
+
 	return S_OK;
 }
 
@@ -20,8 +22,23 @@ void openingScene::release()
 
 void openingScene::update()
 {
+	static int count = 0;
+
+	if (count % 2 == 0 && _index <= 100) _index++;
+
+	if (_index == 100)
+	{
+		_index++;
+		_waitTime = TIMEMANAGER->getWorldTime();
+	}
+
+	if (_index >= 101 && TIMEMANAGER->getWorldTime() >= _waitTime + 3)
+		SCENEMANAGER->changeScene("¿£µù");
+
+	count++;
 }
 
 void openingScene::render()
 {
+	opening->frameRender(getMemDC(), 0, 0, _index, 0);
 }
--- openingScene.h
+++ openingScene.h
@@ -5,7 +5,11 @@ class openingScene : public gameNode
 {
 private:
 
+	int _index;					//프레임 이미지 사용 용도 변수
 
+	image* opening;
+
+	int _waitTime;				// 마지막 대기 시간
 
 public:
 	openingScene();

7c2711cc1220f7f33d91cf82418e817776f92064
only4drone <ljh201410@gmail.com>
2021-09-04 00:56:05 +0900

    uimanager 코드 일부 수정

--- uiManager.h
+++ uiManager.h
@@ -5,10 +5,10 @@
 class uiManager : public singletonBase<uiManager>
 {
 private:
-	int shopCnt = 0;
-	int buyCnt = 0;
-	bool shopWindow = false;
-	bool buyWindow = false;
+	int shopCnt = 0;							//상점 메뉴 선택지
+	int buyCnt = 0;								//아이템 메뉴 선택지
+	bool shopWindow = false;					//상점 메뉴창 띄울지 안띄울지
+	bool buyWindow = false;						//아이템 메뉴창 띄울지 안띄울지
 
 public:
 	uiManager();
